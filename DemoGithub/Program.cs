ffa82f91cbd1c096defa877a0c414851d72d91f2
hv <hv@PM304-GV>
2024-01-16 19:59:10 +0700

    Add project files.

---
+++ DemoGithub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGithub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //demo Github, Gitlab
+            //code here
+        }
+    }
+}
